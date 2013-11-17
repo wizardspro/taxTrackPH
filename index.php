c4c01ff5a8e66ebd9e25c17892574254f1b70bac
Alex <alex@alex-AOD257.(none)>
2013-11-17 09:39:24 +0800

    created header

--- index.php
+++ index.php
@@ -19,96 +19,24 @@ session_start();
         <link href="assets/css/template.css" rel="stylesheet" media="screen">
     </head>
     <body>
-        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="index.php">Tax Track PH</a>
-                </div>
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse navbar-ex1-collapse">
-                    <ul class="nav navbar-nav navbar-right">	
-                        <li><a href="taxcalc.php">Where's my money?</a></li>
-                        <li><a href="budgetcalc.php">Let me do it</a></li>
-                        <li><a href="releases.php">I'm watching yah</a></li>
-                        <?php if (isset($_SESSION['userInfo']['username']) &&
-                			!empty($_SESSION['userInfo']['username'])) : ?>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Username <b class="caret"></b></a>
-                            <ul class="dropdown-menu">
-                            <li><a href="#">Settings</a></li>
-                            <li><a href="logout.php">Logout</a></li>
-                            </ul>
-                            </li>
-                            <?php else: ?>   
-                        <li><button onClick="window.location = 'login.php'" type="button" class="btn btn-primary navbar-btn login_btn">Log in <span class="glyphicon glyphicon-log-in"></span></button></li>
-                    <?php endif; ?>   
-                    </ul>
-                </div>
-                <!-- /.navbar-collapse -->
-            </div>
-            <!-- /.container -->
-        </nav>
+        <?php include "navbar.php" ?>
         <div class="banner">
             <div class="container">
             </div>
         </div>
         <div class="container">
             <div class="row" style="margin-top: 30px;">
-                <div class="col-md-4 text-center">
-                    <img style="width: 140px; height: 140px;" class="img-round" src="assets/img/con-256.png" data-src="holder.js/140x140" alt="140x140">
-                    <h2>Make Donations</h2>
-                    <p>The quick brown fox jumps over the lazy dogs.</p>
-                    <p><a class="btn btn-default" href="about-donations.php">View details »</a></p>
-                </div>
-                <!-- /.col-lg-4 -->
-                <div class="col-md-4 text-center">
-                    <img style="width: 140px; height: 140px;" class="img-round" src="assets/img/yt.png" data-src="holder.js/140x140" alt="140x140">
-                    <h2>Share Knowledge</h2>
-                    <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-                    <p><a class="btn btn-default" href="about-channels.php">View details »</a></p>
-                </div>
-                <!-- /.col-lg-4 -->
-                <div class="col-md-4 text-center">
-                    <img style="width: 140px; height: 140px;" class="img-round" src="assets/img/map.png" data-src="holder.js/140x140" alt="140x140">
-                    <h2>Locate Schools</h2>
-                    <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-                    <p><a class="btn btn-default" href="#">View details »</a></p>
-                </div>
-                <!-- /.col-lg-4 -->
             </div>
-            <hr>
-            <br>
-            <br>
-            <br>
-            <br><br>
-            <br>
-            <br>
-            <br><br>
-            <br>
-            <br>
-            <br><br>
-            <br>
-            <br>
-            <br><br>
-            <br>
-            <br>
-            <br><br>
-            <br>
-            <br>
-            <br>
+            <div id="disqus_thread"></div>
         </div>
         <!-- /container -->
 
 <!-- disqus plugin -->
 
+	
+    
 
-    <div id="disqus_thread"></div>
+
     <script type="text/javascript">
         /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
         var disqus_shortname = 'taxtrackph'; // required: replace example with your forum shortname
---
+++ module2.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title> TaxWatch - taxTrackPH , See and Calculate where thus your tax 
+            going. 
+        </title>
+        <!-- Bootstrap core CSS -->
+        <link href="assets/css/bootstrap.css" rel="stylesheet">
+        <!-- Add custom CSS here -->
+        <link href="assets/css/modern-business.css" rel="stylesheet">
+        <link href="assets/css/index.css" rel="stylesheet" media="screen">
+        <link href="assets/css/template.css" rel="stylesheet" media="screen">
+
+          <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
+    </head>
+    <body>
+        <?php include "navbar.php" ?>
+        <div class="banner">
+            <div class="container">
+                <div style="font-size:60px;box-shadow:0px 5px 15px #000;background-color:#fff;padding:10px;font-family:sans;opacity:0.8;" class="pull-right">Do it your own</div>
+            </div>
+        </div>
+        <div class="container" id="site_content">
+            <div class="col-md-4 ">
+                <button class="btn btn-warning btn-block btn-lg">
+                    Do it Your Own
+                </button>
+                <button class="btn btn-default btn-block">
+                    Overview
+                </button>
+                <div class="panel panel-default"style="margin-top:10px;">
+                    <table class="table table-hover">
+                        <tbody>
+                            <tr>
+                                <td><a href="#">something</a></td>
+                            </tr>
+                            <tr>
+                                <td><a href="#">something</a></td>
+                            </tr>
+                            <tr>
+                                <td><a href="#">something</a></td>
+                            </tr>
+                            <tr>
+                                <td><a href="#">something</a></td>
+                            </tr>
+                            <tr>
+                                <td><a href="#">something</a></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="col-md-8" style="display:none;">
+                <div id="chart_div" style="height: 400px;"></div>
+                <div class=" panel panel-warning">
+                    <div class="panel-heading">
+                        Top Rated
+                    </div>
+                    <hr>
+                </div>
+                <div class=" panel panel-success">
+                    <div class="panel-heading">
+                        Most Popular
+                    </div>
+                    <hr>
+                </div>
+                <div class=" panel panel-danger">
+                    <div class="panel-heading">
+                        Most Hated
+                    </div>
+                    <hr>
+                </div>
+            </div>
+            <div class="col-md-8">
+                <div id="chart_div2" style="height:450px;"></div>
+                <div class=" panel panel-default">
+                    <table class="table table-striped table-bordered 
+                                tablesorter" id="tbl_bySectors">
+                        <thead>
+                            <tr>
+                                <th class="col-md-3">Sectors</th>
+                                <th class="col-md-3 text-center">Budget(%)
+                                </th>
+                                <th class="col-md-3 text-center">Percent</th>
+                                <th class="col-md-3 text-center">New</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Social Services</td>
+                                <td align="center"> P 1221 (37.2%)</td>
+                                <td><input type="number" min="0.00" step="0.01" max="100.00" value="0" id="SS" class="form-control"></td>
+                                <td align="center">P 1232</td>
+                            </tr>
+                            <tr>
+                                <td>Economic Services</td>
+                                <td align="center"> P 1221 (26.0%)</td>
+                                <td><input type="number" min="0.00" step="0.01" max="100.00" value="0" id="ES" class="form-control"></td>
+                                <td align="center">P 1232</td>
+                            </tr>
+                            <tr>
+                                <td>General Public Services</td>
+                                <td align="center"> P 1221 (16.1%)</td>
+                                <td><input type="number" min="0.00" step="0.01" max="100.00" value="0" id="GPS" class="form-control"></td>
+                                <td align="center">P 1232</td>
+                            </tr>
+                            <tr>
+                                <td>Debt Burden</td>
+                                <td align="center"> P 1221 (16.6%)</td>
+                                <td><input type="number" min="0.00" step="0.01" max="100.00" value="0" id="DB" class="form-control"></td>
+                                <td align="center">P 1232</td>
+                            </tr>
+                            <tr>
+                                <td>Defense</td>
+                                <td align="center"> P 1221 (4.1%)</td>
+                                <td><input type="number" min="0.00" step="0.01" max="100.00" value="0" id="D" class="form-control"></td>
+                                <td align="center">P 1232</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <button class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+
+    <div class="footerBanner">
+            <div class="container" style="height:230px;">
+                <div class="col-md-3 col-sm-4 col-xs-6">
+                    <h4><span class="glyphicon glyphicon-thumbs-up"></span> Social</h4>
+                    <hr class="style-two">
+                    <a href="#">Facebook</a>
+                    <br>
+                    <a href="#">Twitter</a>
+                </div>
+                <div class="col-md-3 col-sm-4 col-xs-6">
+                    <h4><span class="glyphicon glyphicon-user"></span> Contact Us</h4>
+                    <hr class="style-two">
+                </div>
+                <div class="col-md-3 col-sm-4 col-xs-6">
+                    <h4><span class="glyphicon glyphicon-user"></span> Developers</h4>
+                    <hr class="style-two">
+                </div>
+                <div class="col-md-3 col-sm-4 col-xs-6">
+                    <h4><span class="glyphicon glyphicon-envelope"></span> Subscribe</h4>
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Email Address">
+                        <span class="input-group-btn">
+                        <button class="btn btn-primary" type="button">Go!</button>
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <div class="copyright">
+                <div class="container text-center">
+                    <small><span class="glyphicon glyphicon-copyright-mark"></span> Copyright 2012. informAksyon. All rights reserved.</small>
+                </div>
+            </div>
+        </div>
+        <!-- Bootstrap core JavaScript -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="assets/js/jquery.js"></script>
+        <script src="assets/js/bootstrap.js"></script>
+          <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+          <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+        <script src="assets/js/jquery-latest.min.js"></script>
+        <script src="assets/js/jquery.tablesorter.min.js"></script>
+
+        <script src="assets/js/modern-business.js"></script>
+        <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+        <script type="text/javascript">
+            google.load("visualization", "1", {packages:["corechart"]});
+              google.setOnLoadCallback(drawChart);
+              google.setOnLoadCallback(drawChart2);
+              function drawChart() {
+                var data = google.visualization.arrayToDataTable([
+                  ['Departments', 'Actual', 'Citizens'],
+                  ['Social Services',  37.2,      30.4],
+                  ['Economic Services',  26.0,      26.1],
+                  ['General Public Services',  16.2,      16],
+                  ['Dept Burden',  16.6,      20],
+                  ['Defense',  4.1,      3],
+                ]);
+
+                var options = {
+                  title: 'Government Budget',
+                  vAxis: {title: 'Departments',  titleTextStyle: {color: 'red'}}
+                };
+
+                var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+                chart.draw(data, options);
+              }
+              function drawChart2() {
+                var data = google.visualization.arrayToDataTable([
+                  ['Departments', 'Actual', 'Citizens','User'],
+                  ['Social Services',  37.2, 30.4, 30.4],
+                  ['Economic Services',  26.0,      26.1, 30.4],
+                  ['General Public Services',  16.2,      16, 30.4],
+                  ['Dept Burden',  16.6,      20, 30.4],
+                  ['Defense',  4.1, 3, 30.4],
+
+                ]);
+
+                var options = {
+                  title: 'Government Budget',
+                  vAxis: {title: 'Departments',  titleTextStyle: {color: 'red'}}
+                };
+
+                var chart = new google.visualization.BarChart(document.getElementById('chart_div2'));
+                chart.draw(data, options);
+              }
+              $('#SS').change(function(){
+                total = parseInt($('#SS').value) + 
+                parseInt($('#ES').value) + parseInt($('#GPS').value) + 
+                parseInt($('#DB').value) + parseInt($('#D').value);
+                alert(total);
+              });
+        </script>
+    </body>
+</html>
